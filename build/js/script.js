758e6711848246e8d2718cd7a829dc843a4d3407
RamunSalazar <52279521+RamunSalazar@users.noreply.github.com>
2021-04-25 20:48:28 +0300

    Внес правки по валидатору

--- build/js/script.js
+++ build/js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 const headerElement = document.querySelector('.header');
 const headerToggleElement = document.querySelector('.header__toggle');
-const inputPhoneElement = document.querySelector('.header__form-phone');
 
 headerElement.classList.remove('header--nojs');
 headerElement.classList.remove('header--open');
@@ -13,18 +12,3 @@ headerToggleElement.addEventListener('click', () => {
     headerElement.classList.add('header--open');
   }
 });
-
-/* const validPhone = (phone) => {
-  let pattern = (/[\D\d]/);
-  return pattern.test(phone);
-};
-
-inputPhoneElement.addEventListener('input', () => {
-  inputPhoneElement.setCustomValidity('');
-  if (validPhone(inputPhoneElement.value)) {
-    inputPhoneElement.setCustomValidity('Телефон не должен содержать букв');
-  } else {
-    inputPhoneElement.setCustomValidity('');
-  }
-  inputPhoneElement.reportValidity();
-}) */
