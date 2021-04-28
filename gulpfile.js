afb50a1ecfd57654f384a9b606e4342304a4f114
RamunSalazar <52279521+RamunSalazar@users.noreply.github.com>
2021-04-28 19:53:46 +0300

    Внес правки по критериям

--- gulpfile.js
+++ gulpfile.js
@@ -87,7 +87,7 @@ gulp.task("copy", function () {
     "source/fonts/*.{woff,woff2}",
     "source/img/**",
     "source/js/**",
-    "source//*.ico"
+    "source/*.ico"
     ], {
       base: "source"
     })
@@ -98,5 +98,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "css", "sprite", "html", "images", "webp", "copy"));
 gulp.task("start", gulp.series("build", "server"));
