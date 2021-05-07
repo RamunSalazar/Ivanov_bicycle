ee2e6992cb2899cc7f10241c68ecbaa6ac5f3542
Mihail <gogol251@mail.ru>
2021-05-07 13:13:22 +0300

    Внес правки согласно баг-листу после ретеста

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@
 const headerElement = document.querySelector('.header');
 const headerToggleElement = document.querySelector('.header__toggle');
 const bodyElement = document.querySelector('body');
+const containerElement = headerElement.querySelector('.container');
 
 headerElement.classList.remove('header--nojs');
 headerElement.classList.remove('header--open');
@@ -10,8 +11,10 @@ headerToggleElement.addEventListener('click', () => {
   if (headerElement.classList.contains('header--open')) {
     headerElement.classList.remove('header--open');
     bodyElement.classList.remove('body--menu');
+    containerElement.classList.remove('container--menu');
   } else {
     headerElement.classList.add('header--open');
-    bodyElement.classList.add('body--menu')
+    bodyElement.classList.add('body--menu');
+    containerElement.classList.add('container--menu');
   }
 });
